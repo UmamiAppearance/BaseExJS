f051e46fdd5c50ce2fddf71204484dced1656963
UmamiAppearance <mail@umamiappearance.eu>
2021-09-01 13:07:58 +0200

    preparing release

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 BaseEx comes from BaseExponent (of n).
 TODO
+
+
+
+// bech32: "qpzry9x8gf2tvdw0s3jn54khce6mua7l",
--- src/BaseEx.js
+++ src/BaseEx.js
@@ -1,4 +1,10 @@
-
+/*
+ * [BaseEx]{@link https://github.com/UmamiAppearance/BaseExJS}
+ *
+ * @version 0.1
+ * @author UmamiAppearance [mail@umamiappearance.eu]
+ * @license GPL-3.0
+ */
 
 class Base16 {
     /*
@@ -162,7 +168,7 @@ class Base32 {
 
         // The length of a base32 string (if padding is used)
         // should not return a remainder if divided by eight.
-        // If they do, missing characters are padded with a "=".
+        // If they do, missing characters are padded with "=".
 
         const missingChars = output.length % 8 * this.padding;
         if (Boolean(missingChars)) {
@@ -199,14 +205,13 @@ class Base32 {
 
         input.split('').map((c) => {
             const index = this.charsets[version].indexOf(c);
-            if (index > -1) {                                                     // -1 is the index if the char is not in the set, "=" e.g. gets ignored
+            if (index > -1) {
                 binaryStr = binaryStr.concat(index.toString(2).padStart(5, "0"));
             }
         });
         
-        // Now the binary string can be (re)grouped 
-        // into regular bytes. Those get plugged into
-        // an Uint8array.
+        // (re)group the binary string into regular 
+        // bytes. Those get plugged into an Uint8array.
 
         const uInt8 = Uint8Array.from(
             binaryStr.match(/.{8}/g).map(bin => 
@@ -338,9 +343,8 @@ class Base64 {
         });
         
 
-        // Now the binary string can be (re)grouped 
-        // into regular bytes. Those get plugged into
-        // an Uint8array.
+        // (re)group the binary string into regular 
+        // bytes. Those get plugged into an Uint8array.
 
         const uInt8 = Uint8Array.from(
             binaryStr.match(/.{8}/g).map(bin => 
@@ -384,7 +388,13 @@ class Base85 {
         (Requires "BaseExUtils")
         
     */
+
     constructor(version="ascii85", input="str", output="str") {
+        /*
+            The charset defined here is used by de- and encoder.
+            This can still be overwritten during the call of the
+            function.
+        */
 
         const asciiChars = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstu";
         this.charsets = {
@@ -396,6 +406,11 @@ class Base85 {
         
         this.IOtypes = ["str", "bytes"];
         this.utils = new BaseExUtils(this);
+
+        // This base85 en-/decoder uses some more
+        // helper functions/vars. Those get append
+        // to the default toolset.
+
         this.expandUtils();
         
         [this.version, this.defaultInput, this.defaultOutput] = this.utils.validateArgs([version, input, output]);
@@ -410,7 +425,7 @@ class Base85 {
             @args:
                 "str"       :  tells the encoder, that input is a string (default)
                 "bytes"     :  tells the encoder, that input is an array
-                "adobe"     :  sets charset to ascii85 + <~padding~> 
+                "adobe"     :  sets charset to ascii85 + <~frame~> 
                 "ascii85"   :  sets charset to this commonly used one
                 "rfc1924"   :  uses the charset (and only the charset) of this april fool
                 "z85"       :  sets the used charset to this variant
@@ -425,11 +440,23 @@ class Base85 {
         // Convert to bytes if input is a string
         const inputBytes = (inputType === "str") ? new TextEncoder().encode(input) : input;
 
+        // Initialize output string and set yet unknown
+        // zero padding to zero.
         let output = "";
         let zeroPadding = 0;
 
+        // iterate over input array in steps of 4 bytes
         for (let i=0, l=inputBytes.length; i<l; i+=4) {
+
+            // build a subarray of 4 bytes
             let subArray = inputBytes.slice(i, i+4);
+
+            // At the very last block of 4 bytes, there 
+            // is a change that the subarray has a length
+            // less than 4. If this is the case, padding is
+            // required. All missing bytes are filled with
+            // zeros. The amout of zeros is stored in
+            // "zeroPadding".
 
             if (subArray.length < 4) {
                 zeroPadding = 4 - subArray.length;
@@ -438,26 +465,45 @@ class Base85 {
                 subArray = paddedArray;
             }
             
+            // The subarray gets converted into a 32-bit 
+            // binary number "n", most significant byte 
+            // first
+
             let n = 0;
             subArray.forEach((b, j) => n += b * this.utils.pow256[j]);
 
+            // A new standard array gets initilized, to
+            // store 5 radix-85 digits  
             const b85Array = new Array();
 
-            let q = n, r;                                              // initialize quotient and remainder
+            // Initialize quotient and remainder for base convertion
+            let q = n, r;
+
+            // Divide n until the remainder is 85 or less
             while (q > 85) {
                 [q, r] = this.utils.divmod(q, 85);
                 b85Array.unshift(r);
             }
+            // Append the reamining quotient to the array
             b85Array.unshift(q);
 
+            // If the lenght of the array is less than 5
+            // it gets filled up with zeros.
             while (b85Array.length < 5) {
                 b85Array.unshift(0);
             }
 
+            // Each base85 digit gets used as an index
+            // to pick a corresponding char from the
+            // charset. The chars get concatinated and
+            // stored in "frame".
             let frame = "";
             b85Array.forEach(
                 charIndex => frame = frame.concat(this.charsets[version][charIndex])
             );
+
+            // For default ascii85, five consecutive null bits
+            // (or "!") are replaced with the letter "z".
 
             if ((frame === "!!!!!") && Boolean(version.match(/adobe|ascii85/))) {
                 output = output.concat("z");   
@@ -466,16 +512,36 @@ class Base85 {
             }
         }
 
+        // If padding was taking place, the amout
+        // of used zeros "zeroPadding" gets cut of
+        // from the outpus string.
+
         output = output.slice(0, output.length-zeroPadding);
+        
+        // Adobes variant gets its <~framing~>
         if (version === "adobe") {
             output = `<~${output}~>`;
         }
         
         if (version === "rfc1924") this.utils.announce();
+        
         return output;
     }
 
     decode(input, ...args) {
+        /* 
+            Decode from base85 string to utf8-string or bytes.
+            -------------------------------------------------
+
+            @input: base85-string
+            @args:
+                "str"       :  tells the encoder, that output should be a string (default)
+                "bytes"     :  tells the encoder, that output should be an array
+                "adobe"     :  sets charset to ascii85 + <~frame~> 
+                "ascii85"   :  sets charset to this commonly used one
+                "rfc1924"   :  uses the charset (and only the charset) of this april fool
+                "z85"       :  sets the used charset to this variant
+        */
 
         // Argument validation and output settings
         args = this.utils.validateArgs(args);
@@ -483,6 +549,7 @@ class Base85 {
         const outputType = this.utils.setIOType(args, "out");
         input = input.replace(/\s/g,'');        //remove all whitespace from input
         
+        // For default ascii85 convert "z" back to "!!!!!"
         if (Boolean(version.match(/adobe|ascii85/))) {
             input = input.replace(/z/g, "!!!!!");
             if (version === "adobe") {
@@ -490,15 +557,34 @@ class Base85 {
             }
         }
 
+        // Convert each char of the input to a radix-85
+        // integer (this is the corresponding index of 
+        // the char from the charset).
+
         const inputBytes = Uint8Array.from(
             input.split('').map(c => this.charsets[version].indexOf(c))
         );
 
-        let uPadding = 0;
+        // Initialize a new default array to store
+        // the converted radix-256 integers. And set 
+        // yet unknown uPadding padding to zero.
+
         let b256Array = new Array();
+        let uPadding = 0;
+
+        // Iterate over the input bytes in steps of 5.
         for (let i=0, l=input.length; i<l; i+=5) {
+            
+            // build a subarray of 5 bytes
             let subArray = inputBytes.slice(i, i+5);
 
+            // If the last subarray has a length less than
+            // five, it gets padded with the highest possible
+            // value, which is 84. The amout of padding is
+            // stored in "uPadding".
+            // ("u" is the corresponding char to 84 in the ascii85
+            // charset).
+            
             if (subArray.length < 5) {
                 uPadding = 5 - subArray.length;
                 const paddedArray = Uint8Array.from(Array(5).fill(84));
@@ -506,24 +592,44 @@ class Base85 {
                 subArray = paddedArray;
             }
             
+            // To store the output chunks, initialize a
+            // new default array.
+
             const subArray256 = new Array();
 
+            // The subarray gets converted into a 32-bit 
+            // binary number "n", most significant byte 
+            // first
             let n = 0;
             subArray.forEach((b, j) => n += b * this.utils.pow85[j]);
 
+            
+            // Initialize quotient and remainder for base convertion
             let q = n, r;
+
+            // Divide n until the remainder is 256 or less
             while (q > 256) {
                 [q, r] = this.utils.divmod(q, 256);
                 subArray256.unshift(r);
             }
+            // Append the reamining quotient to the array
             subArray256.unshift(q);
             
+            // If the lenght of the array is less than 4
+            // it gets filled up with zeros.
+
             while (subArray256.length < 4) {
                 subArray256.unshift(0);
             }
             
+            // The subarray gets concatianted with the
+            // main array.
             b256Array = b256Array.concat(subArray256);
         }
+
+        // Convert default array to tyed uInt8 array.
+        // The amount bytes according to the padded ues
+        // is left behind.
 
         const uInt8 = Uint8Array.from(b256Array.slice(0, b256Array.length-uPadding));
 
@@ -569,51 +675,82 @@ class Base85 {
 
 
 class BaseExUtils {
+    /*
+        Utilities for every BaseEx class.
+        The main purpose is argument validation.
+    */
+
     constructor(main) {
-        this.main = main;
 
-        if ("charsets" in main) {
+        // Store the calling class in this.root
+        // for accessability.
+        this.root = main;
 
-            this.addCharset = (name, charset) => {
+        // If charsets are uses by the parent class,
+        // add extra functions for the user.
+        if ("charsets" in main) this.charsetUserToolsConstructor();
+    }
+
+    charsetUserToolsConstructor() {
+        /*
+            Contructor for the ability to add a charset and 
+            change the default version.
+        */
+
+        this.root.addCharset = (name, charset) => {
+            /*
+                Save method to add a charset.
+                ----------------------------
+
+                @name: string that represents the key for the new charset
+                @charset: string, array or Set of chars - the lenght must fit to the accoding class 
+            */
                 
-                if (typeof name !== "string") {
-                    throw new TypeError("The charset name must be a string.");
-                }
-    
-                const setLen = parseInt(this.main.constructor.name.replace(/[^0-9]/g, ""), 10);
-                let inputLen = setLen;
-                
-                if (typeof charset === "string" || Array.isArray(charset)) {
-                    inputLen = charset.length;
-                    charset = new Set(charset);
-                } else if (typeof charset !== "set") {
-                    throw new TypeError("The charset must be one of the types:\n'str', 'set', 'array'.");
-                }
-                
-                if (charset.size === setLen) {
-                    charset = [...charset].join("");
-                    this.main.charsets[name] = charset;
-                    console.log("New charset added.");
-                } else if (inputLen === setLen) {
-                    throw new Error("There were repetitive letters found in your charset. Make sure each char is unique.");
-                } else {
-                    throw new Error(`The the length of the charset must be ${setLen}.`);
-                }
+            if (typeof name !== "string") {
+                throw new TypeError("The charset name must be a string.");
             }
 
+            // Get the approprite length for the charset
+            // from the name of the parent function.
+            // (Which is a little hacky...)
+
+            const setLen = parseInt(this.root.constructor.name.replace(/[^0-9]/g, ""), 10);
+            let inputLen = setLen;
+            
+            if (typeof charset === "string" || Array.isArray(charset)) {
+                
+                // Store the input length of the input
+                inputLen = charset.length;
+                
+                // Convert to "Set" -> eliminate dublicates
+                // If duvlicates are found the length of the
+                // Set and the length of the initial input
+                // differ.
+
+                charset = new Set(charset);
+
+            } else if (typeof charset !== "set") {
+                throw new TypeError("The charset must be one of the types:\n'str', 'set', 'array'.");
+            }
+            
+            if (charset.size === setLen) {
+                charset = [...charset].join("");
+                this.root.charsets[name] = charset;
+                console.log(`New charset added with the name '${name}' added and ready to use`);
+            } else if (inputLen === setLen) {
+                throw new Error("There were repetitive chars found in your charset. Make sure each char is unique.");
+            } else {
+                throw new Error(`The the length of the charset must be ${setLen}.`);
+            }
         }
+
+        // Save method (argument gets validated) to 
+        // change the default version.
+        this.root.setDefaultVersion = (version) => [this.root.version] = this.validateArgs([version]);
     }
 
-    makeListing(args) {
+    makeArgList(args) {
         return args.map(s => `'${s}'`).join(", ")
-    }
-
-    setDefaultVersion(version) {
-        if ("version" in this.main) {
-            [this.main.version] = this.validateArgs([version]);
-        } else {
-            this.warning("This class has no charset-object. Default cannot be set.");
-        }
     }
 
     setIOType(args, IO) {
@@ -626,21 +763,21 @@ class BaseExUtils {
         } else if (args.includes("str")) { 
             type = "str";
         } else {
-            type = (IO === "in") ? this.main.defaultInput : this.main.defaultOutput;
+            type = (IO === "in") ? this.root.defaultInput : this.root.defaultOutput;
         }
 
         return type;
     }
 
     getVersion(args) {
-        /*listArgs
+        /*
             Test which version (charset) shoult be used.
             Sets either the default or overwrites it if
             requested.
         */
-        let version = this.main.version;
+        let version = this.root.version;
         args.forEach(arg => {
-            if (arg in this.main.charsets) {
+            if (arg in this.root.charsets) {
                 version = arg; 
             }
         })
@@ -656,19 +793,19 @@ class BaseExUtils {
         let validArgs;
         const loweredArgs = new Array();
 
-        if ("charsets" in this.main) {
-            versions = Object.keys(this.main.charsets);
-            validArgs = [...this.main.IOtypes, ...versions];
+        if ("charsets" in this.root) {
+            versions = Object.keys(this.root.charsets);
+            validArgs = [...this.root.IOtypes, ...versions];
         } else {
-            validArgs = this.main.IOtypes;
+            validArgs = this.root.IOtypes;
         }
 
         if (Boolean(args.length)) {
             args.forEach(arg => {
                 arg = String(arg).toLowerCase();
                 if (!validArgs.includes(arg)) {
-                    const versionHint = (versions) ? `The options for version (charset) are:\n${this.makeListing(versions)}\n\n` : "";
-                    throw new TypeError(`'${arg}'\n\nValid arguments for in- and output-type are:\n${this.makeListing(this.main.IOtypes)}\n\n${versionHint}Traceback:`);
+                    const versionHint = (versions) ? `The options for version (charset) are:\n${this.makeArgList(versions)}\n\n` : "";
+                    throw new TypeError(`'${arg}'\n\nValid arguments for in- and output-type are:\n${this.makeArgList(this.root.IOtypes)}\n\n${versionHint}Traceback:`);
                 }
                 loweredArgs.push(arg);
             });
@@ -719,6 +856,3 @@ class BaseEx {
 }
 
 //export {Base16, Base32, Base64, Base85, BaseEx}
-
-
-// bech32: "qpzry9x8gf2tvdw0s3jn54khce6mua7l",
