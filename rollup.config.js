faa63ea9d936488d2eab547890b8640a39ac6729
UmamiAppearance <mail@umamiappearance.eu>
2021-09-26 21:36:49 +0200

    rollup + minifiying

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import { terser } from "rollup-plugin-terser";
+
 export default {
     input: "src/BaseEx.js",
     output: [ 
@@ -7,9 +9,21 @@ export default {
             file: "dist/BaseEx.js"
         },
         {   
+            format: "iife",
+            name: "BaseEx",
+            file: "dist/BaseEx.min.js",
+            plugins: [terser()]
+        },
+        {   
             format: "es",
             name: "BaseEx",
             file: "dist/BaseEx.esm.js"
+        },
+        {   
+            format: "es",
+            name: "BaseEx",
+            file: "dist/BaseEx.esm.min.js",
+            plugins: [terser()]
         },
     ]
 };
