e89c5cf7bcc95ba661eb02416f5b4242884a8a7b
UmamiAppearance <mail@umamiappearance.eu>
2021-09-26 22:45:48 +0200

    new build

--- dist/BaseEx.js
+++ dist/BaseEx.js
@@ -76,7 +76,7 @@ var BaseEx = (function (exports) {
            input = String(input).replace(/^0x/, '');
            
            // Ensure even number of characters
-           if (Boolean(input.length % 2)) {
+           if (input.length % 2) {
                input = "0".concat(input);
            }
            
@@ -183,7 +183,7 @@ var BaseEx = (function (exports) {
 
            // If the input is unpadded, pad it.
            const missingChars = input.length % 8;
-           if (Boolean(missingChars)) {
+           if (missingChars) {
                input = input.padEnd(input.length + 8-missingChars, "=");
            }
 
@@ -289,7 +289,7 @@ var BaseEx = (function (exports) {
     
            // If the input is unpadded, pad it.
            const missingChars = input.length % 4;
-           if (Boolean(missingChars)) {
+           if (missingChars) {
                input = input.padEnd(input.length + 4-missingChars, "=");
            }
 
@@ -386,8 +386,8 @@ var BaseEx = (function (exports) {
 
            // Initialize the replacing of null bytes for ascii85
            let replacer = null;
-           if (Boolean(version.match(/adobe|ascii85/))) {
-               replacer = (frame, zPad) => (!Boolean(zPad) && frame === "!!!!!") ? "z" : frame;
+           if (version.match(/adobe|ascii85/)) {
+               replacer = (frame, zPad) => (!zPad && frame === "!!!!!") ? "z" : frame;
            }
 
            // Convert to Base85 string        
@@ -428,7 +428,7 @@ var BaseEx = (function (exports) {
            input = input.replace(/\s/g,'');        //remove all whitespace from input
            
            // For default ascii85 convert "z" back to "!!!!!"
-           if (Boolean(version.match(/adobe|ascii85/))) {
+           if (version.match(/adobe|ascii85/)) {
                input = input.replace(/z/g, "!!!!!");
                if (version === "adobe") {
                    input = input.replace(/^<~|~>$/g, "");
@@ -451,6 +451,7 @@ var BaseEx = (function (exports) {
                if (!debug) {
                    const date = new Date();
                    if (date.getMonth() === 3 && date.getDate() === 1) {
+                       // eslint-disable-next-line no-useless-escape
                        console.log("         __\n _(\\    |@@|\n(__/\\__ \\--/ __\n   \\___|----|  |   __\n       \\ }{ /\\ )_ / _\\\n       /\\__/\\ \\__O (__\n      (--/\--)    \\__/\n      _)(  )(_\n     `---''---`");
                    } else {
                        const ts = date.getTime();
@@ -585,7 +586,7 @@ var BaseEx = (function (exports) {
            // If the bitCount is not zero at the end,
            // calculate quotient and remainder of 91
            // once more.
-           if (Boolean(bitCount)) {
+           if (bitCount) {
                let q, r;
                [q, r] = this.utils.divmod(n, 91);
 
@@ -629,7 +630,7 @@ var BaseEx = (function (exports) {
            // if the length of the input string is odd.
 
            let odd = false;
-           if (Boolean(l % 2)) {
+           if (l % 2) {
                odd = true;
                l--;
            }
@@ -660,6 +661,7 @@ var BaseEx = (function (exports) {
                    bitCount -= 8;
                } while (bitCount > 7);
            }
+
            // Calculate the last byte if the input is odd
            // and add it
            if (odd) {
@@ -778,7 +780,7 @@ var BaseEx = (function (exports) {
                );
 
                // Ascii85 is replacing four consecutive "!" into "z"
-               if (Boolean(replacer)) {
+               if (replacer) {
                    frame = replacer(frame, zeroPadding);
                }
 
@@ -972,7 +974,7 @@ var BaseEx = (function (exports) {
 
                    charset = new Set(charset);
 
-               } else if (typeof charset !== "set") {
+               } else if (typeof charset !== "object") { // TODO: unprecise test
                    throw new TypeError("The charset must be one of the types:\n'str', 'set', 'array'.");
                }
                
@@ -1046,7 +1048,7 @@ var BaseEx = (function (exports) {
                validArgs = this.root.IOtypes;
            }
 
-           if (Boolean(args.length)) {
+           if (args.length) {
                args.forEach(arg => {
                    arg = String(arg).toLowerCase();
                    if (!validArgs.includes(arg)) {
