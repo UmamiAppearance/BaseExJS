ea37e11e9f78c4b6e80cb75af1491c3f283db876
UmamiAppearance <mail@umamiappearance.eu>
2022-02-09 15:40:06 +0100

    multiple exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,58 @@
 import { terser } from "rollup-plugin-terser";
 
-export default {
-    input: "src/BaseEx.js",
-    output: [ 
-        {   
-            format: "iife",
-            name: "BaseEx",
-            file: "dist/BaseEx.iife.js"
-        },
-        {   
-            format: "iife",
-            name: "BaseEx",
-            file: "dist/BaseEx.iife.min.js",
-            plugins: [terser()]
-        },
-        {   
-            format: "es",
-            name: "BaseEx",
-            file: "dist/BaseEx.esm.js"
-        },
-        {   
-            format: "es",
-            name: "BaseEx",
-            file: "dist/BaseEx.esm.min.js",
-            plugins: [terser()]
-        },
-    ]
-};
+export default [
+    {
+        input: "src/base-ex.js",
+        output: [ 
+            {   
+                format: "iife",
+                name: "BaseEx",
+                file: "dist/base-ex.iife.js"
+            },
+            {   
+                format: "iife",
+                name: "BaseEx",
+                file: "dist/base-ex.iife.min.js",
+                plugins: [terser()]
+            },
+            {   
+                format: "es",
+                name: "BaseEx",
+                file: "dist/base-ex.esm.js"
+            },
+            {   
+                format: "es",
+                name: "BaseEx",
+                file: "dist/base-ex.esm.min.js",
+                plugins: [terser()]
+            },
+        ]
+    }, {
+        input: "src/base-16.js",
+        output: [ 
+            {   
+                format: "iife",
+                name: "Base16",
+                file: "dist/components/base-16.iife.js"
+            },
+            {   
+                format: "iife",
+                name: "Base16",
+                file: "dist/components/base-16.iife.min.js",
+                plugins: [terser()]
+            },
+            {   
+                format: "es",
+                name: "Base16",
+                file: "dist/components/base-16.esm.js"
+            },
+            {   
+                format: "es",
+                name: "Base16",
+                file: "dist/components/base-16.esm.min.js",
+                plugins: [terser()]
+            },
+        ]
+    },
+];
+
