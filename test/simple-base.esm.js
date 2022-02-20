2e77c388513942007b54e0c4cac7e8d2babe0863
UmamiAppearance <mail@umamiappearance.eu>
2022-02-20 21:37:42 +0100

    optimized simple base tests

--- test/simple-base.esm.js
+++ test/simple-base.esm.js
@@ -1,49 +1,18 @@
 import { SimpleBase } from "../src/base-ex.js";
 
-const Base10 = new SimpleBase(10);
+function simpleBaseTests() {
 
-function strToBaseX(input, encoder) {
-    const endianness = (encoder.littleEndian) ? "LE" : "BE";
-    const b10Integer = BigInt(Base10.encode(input, endianness));
-    return b10Integer.toString(encoder.converter.radix);
-}
-
-function numberConversions() {
-
+    const Base10 = new SimpleBase(10);
     const randInt = (max, min=0) => BigInt(Math.floor(Math.random() * (max - min) + min));
-   
-    for (let radix=2; radix<37; radix++) {
+    const strToBase = (input, encoder) => {
+        const endianness = (encoder.littleEndian) ? "LE" : "BE";
+        const b10Integer = BigInt(Base10.encode(input, endianness));
+        return b10Integer.toString(encoder.converter.radix);
+    };
+
+    for (let radix=2; radix<=36; radix++) {
         
         const baseConverter = new SimpleBase(radix);
-
-        const testRange = (input) => {
-            //console.log("---------\n");
-
-            const expected = input.toString(radix);
-            //console.log("Input: ", input);
-            //console.log("Expected: ", expected);
-
-            const output = baseConverter.encode(input);
-            //console.log("Output: ", output);
-
-            const passedEnc = output === expected;
-            let passedDec = false;
-            if (passedEnc) {
-                const backDecoded = baseConverter.decode(output, "number");
-                //console.log("BackDec: ", backDecoded);
-
-                passedDec = backDecoded === input;
-
-            }
-
-            //console.log("Passed: ", passedEnc, "/", "Passed: ", passedDec);
-            if (!(passedEnc === true && passedDec === true)) {
-                console.log("RADIX", radix);
-                throw new Error("BUG!");
-            }
-            return [passedEnc, passedDec];
-    
-        }
 
         console.log("---------\nRADIX", radix);
      
@@ -61,26 +30,57 @@ function numberConversions() {
                         nn = Number(nn);
                     } 
                     
-                    testRange(nn);
+                    const expected = nn.toString(radix);
+                    //console.log("Input: ", nn);
+                    //console.log("Expected: ", expected);
+
+                    const output = baseConverter.encode(nn);
+                    //console.log("Output: ", output);
+
+                    const passedEnc = output === expected;
+                    let passedDec = false;
+                    if (passedEnc) {
+                        const backDecoded = baseConverter.decode(output, "number");
+                        //console.log("BackDec: ", backDecoded);
+
+                        passedDec = backDecoded === nn;
+
+                    }
+
+                    //console.log("Passed: ", passedEnc, "/", "Passed: ", passedDec);
+                    if (!(passedEnc === true && passedDec === true)) {
+                        console.log("RADIX", radix);
+                        throw new Error("BUG!");
+                    }
+                    console.log([passedEnc, passedDec]);
                 }
             
             }
 
         }
 
+        let helloInput = "";
+        "Hello World!!!".split("").forEach(c => {
+            helloInput += c;
+            const expected = strToBase(helloInput, baseConverter);
+            const output = baseConverter.encode(helloInput);
 
-    const x = strToBaseX("Hello World!!!", baseConverter);
-    const y = baseConverter.encode("Hello World!!!");      
-    
-    console.log("HW", x);
-    console.log("HW", y);
-    
-    console.log(x === y);
-    if (x !== y) throw new Error("BUG !!!");
+            const passedEnc = output === expected;
+            let passedDec = false;
+            
+            if (passedEnc) {
+                const backDecoded = baseConverter.decode(output, "str");
+                console.log("BackDec: ", backDecoded);
 
+                passedDec = backDecoded === helloInput;
+
+            }
+
+            console.log([passedEnc, passedDec]);
+
+        });
     }
 
 }
 
-
-numberConversions();
+simpleBaseTests();
