3228dbe2a7d2c27194162767b43a6ddf331cae08
UmamiAppearance <mail@umamiappearance.eu>
2022-09-27 22:20:46 +0200

    v0.5.0

--- test/ecoji-orig.test.js
+++ test/ecoji-orig.test.js
@@ -1,64 +1,91 @@
 
-import { readdir, readFile } from "fs/promises";
+import { readFile, readdir } from "fs/promises";
 import { Ecoji } from "base-ex";
 import test from "ava";
 
-const endDecodeTest = test.macro(async (t, input, expected, base, ...args) => {
-    const output = base.encode(input, ...args);
+// macros
+const enDecodeTest = test.macro((t, input, expected, ecoji) => {
+    const output = ecoji.encode(input);
     t.is(output, expected);
-    t.deepEqual(base.decode(output, ...args), input);
+    t.deepEqual(ecoji.decode(output), input);
+});
+
+const decodeTest = test.macro((t, input, expected, ecoji, ...args) => {
+    t.is(ecoji.decode(input, ...args), expected);
+});
+
+const errorTest = test.macro((t, input, ecoji) => {
+    t.throws(() => ecoji.decode(input));
 });
 
 
-console.log(process.cwd());
-
+// declarations
 const path = "./test/fixtures/ecoji/";
 const files = await (readdir(path));
-
-const plainFilesA = files.filter(f => (/\.plain$/).test(f));
-//const plainFilesB = files.filter(f => (/\.plaind$/).test(f));
-//const garbage = files.filter(f => (/\.garbage$/).test(f));
-
 const ecojiV1 = new Ecoji("emojis_v1", "uint8");
 const ecojiV2 = new Ecoji("emojis_v2", "uint8");
 
-const testsA = plainFilesA.map(async plainFile => {
-    const bareName = plainFile.slice(0, -6);
-    
-    const input = new Uint8Array(await readFile(`${path}${plainFile}`));
-    
-    const expectedV1 = await readFile(`${path}${bareName}.ev1`, "utf-8");
-    const expectedV2 = await readFile(`${path}${bareName}.ev2`, "utf-8");
-    
-    return {
-        input,
-        expectedV1,
-        expectedV2
-    }
-    
+
+// test group file lists
+const plainFilesA = files.filter(f => (/\.plain$/).test(f));
+const aLen = plainFilesA.length;
+
+const plainFilesB = files.filter(f => (/\.plaind$/).test(f));
+const bLen = plainFilesB.length;
+
+const garbage = files.filter(f => (/\.garbage$/).test(f));
+const gLen = garbage.length;
+
+
+// test data generation
+const dataA = await Promise.all(
+    plainFilesA.map(async plainFile => {
+        const bareName = plainFile.slice(0, -6);
+        
+        const input = new Uint8Array(await readFile(`${path}${plainFile}`));
+        
+        const expectedV1 = await readFile(`${path}${bareName}.ev1`, "utf-8");
+        const expectedV2 = await readFile(`${path}${bareName}.ev2`, "utf-8");
+        
+        return {
+            input,
+            expectedV1,
+            expectedV2
+        } 
+    })
+);
+
+const dataB = await Promise.all(
+    plainFilesB.map(async plainFile => {
+        const bareName = plainFile.slice(0, -7);
+        
+        const input = await readFile(`${path}${bareName}.enc`, "utf-8");
+        const expected = await readFile(`${path}${plainFile}`, "utf-8");
+
+        return {
+            input,
+            expected
+        }
+    })
+);
+
+const dataC = await Promise.all(
+    garbage.map(
+        async plainFile => readFile(`${path}${plainFile}`)
+    )
+);
+
+
+// tests
+dataA.forEach((data, i) => {
+    test(`En- and decoding for Ecoji Version 1, Test: [${i+1}|${aLen}]`, enDecodeTest, data.input, data.expectedV1, ecojiV1);
+    test(`En- and decoding for Ecoji Version 2, Test: [${i+1}|${aLen}]`, enDecodeTest, data.input, data.expectedV2, ecojiV2); 
 });
 
-const testAResults = await Promise.all(testsA);
-
-testAResults.forEach((t, i) => {
-    test(`En- and decoding for Ecoji Version 1 (test: ${i})`, endDecodeTest, t.input, t.expectedV1, ecojiV1);
-    test(`En- and decoding for Ecoji Version 2 (test: ${i})`, endDecodeTest, t.input, t.expectedV2, ecojiV2); 
+dataB.forEach((data, i) => {
+    test(`Decode sample data for both versions, Test: [${i+1}|${bLen}]`, decodeTest, data.input, data.expected, ecojiV2, "str"); 
 });
 
-/*
-for (const plainFile of plainFilesB) {
-    const bareName = plainFile.slice(0, -7);
-    
-    const input = await readFile(`${path}${bareName}.enc`);
-    const expected = await readFile(`${path}${plainFile}`, "utf-8");
-
-    const output = ecojiV2.decode(input, "str");
-
-}
-
-for (const plainFile of garbage) {
-    const input = await readFile(`${path}${plainFile}`);
-    //const output = ecojiV2.decode(input, "str");
-
-}
-*/
+dataC.forEach((data, i) => {
+    test(`Garbage input > throw Error, Test: [${i+1}|${gLen}]`, errorTest, data, ecojiV2); 
+});
